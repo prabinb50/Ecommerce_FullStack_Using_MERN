71f5750e8f5ef951cb42fc0864503270329f4820
Prabin Joshi <joshiprabin17@gmail.com>
2025-02-20 12:50:52 +0545

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,42 +1,112 @@
 import express from "express";
-import mongoose from "mongoose";
-import mangoose from "mongoose";
+import mongoose from "mongoose"
 
 // configure the server
 const app = express();
+
+// middlewware
 app.use(express.json());
+
+
+
 
 // connect to mongoDB database
 try {
-    mangoose.connect("mongodb+srv://joshiprabin17:auB3An6tWfrvnbJQ@cluster0.cthkw.mongodb.net/ecommerce-db?retryWrites=true&w=majority&appName=Cluster0");
+    mongoose.connect("mongodb+srv://joshiprabin17:GLlYWyKcNvqoeTMY@cluster0.zzdzy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("MongoDB Connection Success");
 } catch (error) {
     console.log("MangoDB Connection Error", error);
 }
 
-// Products Schema (items for the product table)
-const productSchema = new mangoose.Schema({
-    name: { type: String, required: true },
+
+
+
+// Table Schema -> Products Schema (items for the product table)
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
     description: { type: String },
     price: { type: Number, required: true },
     previousPrice: { type: Number, required: true },
     imageUrl: { type: String, required: true },
-    categoty: { type: String, required: true }
+    category: { type: String, required: true }
 });
 
 // Make Prouct Table (Model)
 const Product = mongoose.model("Product", productSchema);
 
+
+
+
 // CRUD Operations For Product
 // 1) Create a product
 app.post("/products", async (req, res) => {
     try {
+        // check if product name already taken or not
+        const productExits = await Product.findOne({
+            name: req.body.name
+        });
+        if (productExits) {
+            return res.status(409).json({
+                message: "Name already taken, please choose different name",
+            })
+        }
+
+
         const newProduct = await new Product(req.body).save();
-        console.log(newProduct);
+        return res.status(201).json({
+            message: "Product created successfully",
+            data: newProduct,
+        });
     } catch (error) {
-        console.log("Something went wrong", error);
+        return res.status(500).json({
+            message: "Internal server error",
+        })
     }
 })
+
+// 2) Read (Get) products
+app.get("/products", async (req, res) => {
+    try {
+        const allProducts = await Product.find();
+        return res.status(200).json({
+            message: "All products fetched successfully",
+            data: allProducts
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal server error",
+        })
+    }
+})
+
+// Read a particular product
+app.get("/products/:id", async (req, res) => {
+    try {
+        const singleProduct = await Product.findById(req.params.id);
+        return res.status(200).json({
+            message: "Single product fetched successfully",
+            data: singleProduct
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal server error",
+        })
+    }
+})
+
+// 3) Update a particular product
+app.patch("/products/:id", async (req, res) => {
+
+})
+
+// 4) Delete a particular product
+app.delete("/products/:id", async (req, res) => {
+
+})
+
+
 
 
 app.get('/', (req, res) => {
@@ -50,4 +120,3 @@ app.get("/students", (req, res) => {
 app.listen(3000, () => {
     console.log("Example app listening on port 3000!");
 })
-
